917b592abc40209020c759282f210692fa6d35fd
vbreguta <146566316+vbreguta@users.noreply.github.com>
2023-11-03 16:27:01 -0500

    Update project2.py

--- project2.py
+++ project2.py
@@ -1,23 +1,7 @@
-""" Application Name: Homework 1
-# Description: (INDEXING AND SLICING ARRAYS) Create an array containing the values 1–15, reshape it 
-                into a 3-by-5 array, then use indexing and slicing techniques to perform each of the following 
-                operations: 
-                a) Select row 2. 
-                b) Select column 4. 
-                c) Select rows 0 and 1. 
-                d) Select columns 2–4. 
-                e) Select the element that is in row 1 and column 4. 
-                f) Select all elements from rows 1 and 2 that are in columns 0, 2 and 4.
 
-# Author: Valeria Breguta 
-# Course Name: Advanced Python
-# Section: CIS-2532-NET02
-# Instructor: Mohammad Morovati
-# Date: 09/06/2023
-"""
 import numpy as np
 
-#Creating the array containing teh values from 1 to 15 
+#Creating the array containing the values from 1 to 15 
 ar = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
 
 #Reshaping the array into a 3 by 5
